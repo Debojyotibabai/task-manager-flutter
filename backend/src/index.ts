f3a9e86714673ef2647773c8726abbdfbfaac73a
Debojyotibabai <debojyoti@appstango.com>
2024-12-23 09:19:50 +0530

    day 3

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
+import authRouter from "./routes/auth";
 
 const app = express();
 
+app.use(express.json());
+app.use("/auth", authRouter);
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Welcome to my app!!!!!!!");
 });
 
 app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+  console.log("Server started on port 8000");
 });
