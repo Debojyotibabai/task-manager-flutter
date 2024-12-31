09aa3f60fac87e3a8022e07adeedd90e7031f5a5
Debojyotibabai <debojyoti@appstango.com>
2024-12-31 16:19:06 +0530

    filter data

--- app/lib/features/home/presentation/widgets/calendar.dart
+++ app/lib/features/home/presentation/widgets/calendar.dart
@@ -172,7 +172,7 @@ class _CalendarState extends State<Calendar> {
                                 : Colors.black,
                             fontSize: 17,
                           ),
-                        )
+                        ),
                       ],
                     ),
                   ),
